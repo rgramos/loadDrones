38f84c2d24e6e3cff02900b95bc6c5b117d8a8e6
develop_men10 <rgramos9310@gmail.com>
2022-03-05 17:00:48 -0500

    drones model and migration

--- loadDrones/drones/models.py
+++ loadDrones/drones/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+from loadDrones.models import AbsSlugTimestamp
+
+
+class Drone(AbsSlugTimestamp):
+    LIGHTWEIGHT = 'Lightweight'
+    MIDDLEWEIGHT = 'Middleweight'
+    CRUISERWEIGHT = 'Cruiserweight'
+    HEAVYWEIGHT = 'Heavyweight'
+    MODEL_CHOICE = [
+        (LIGHTWEIGHT, 'Lightweight'),
+        (MIDDLEWEIGHT, 'Middleweight'),
+        (CRUISERWEIGHT, 'Cruiserweight'),
+        (HEAVYWEIGHT, 'Heavyweight')
+    ]
+
+    IDLE = 'IDLE'
+    LOADING = 'LOADING'
+    LOADED = 'LOADED'
+    DELIVERING = 'DELIVERING'
+    DELIVERED = 'DELIVERED'
+    RETURNING = 'RETURNING'
+    STATE_CHOICE = [
+        (IDLE, 'IDLE'),
+        (LOADING, 'LOADING'),
+        (LOADED, 'LOADED'),
+        (DELIVERING, 'DELIVERING'),
+        (DELIVERED, 'DELIVERED'),
+        (RETURNING, 'RETURNING')
+    ]
+
+    serial_number = models.CharField(max_length=100)
+    weight_limiter = models.FloatField()
+    battery_capacity = models.IntegerField()
+    model = models.CharField(max_length=255, choices=MODEL_CHOICE)
+    state = models.CharField(max_length=255, choices=STATE_CHOICE)
+
+    class Meta:
+        db_table = 'drone'
+        verbose_name = 'Drone'
+        verbose_name_plural = 'Drones'
+
+    def __str__(self):
+        return self.serial_number
